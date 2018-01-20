63abb92ebc4f8cb49891e8ec657c579831d581de
alexknipfer <alexanderknipfer@gmail.com>
2018-01-20 11:32:35 -0600

    Pull logic from register and use auth model

--- api/mutations/register.js
+++ api/mutations/register.js
@@ -5,29 +5,7 @@ const ObjectId = require('mongodb').ObjectID
 const register = async (
   root,
   { email, password, firstName, lastName },
-  { db }
-) => {
-  const userCollection = db.collection('users')
-  const duplicateUser = await userCollection.find({ email }).count()
-
-  if (duplicateUser >= 1) throw new Error('User already exists.')
-  else {
-    const saltRounds = 10
-    const hash = bcrypt.hashSync(password, saltRounds)
-
-    const userDetails = {
-      email,
-      password: hash,
-      firstName,
-      lastName
-    }
-
-    const { insertedId } = await userCollection.insertOne(userDetails)
-
-    const { _id: userId } = await userCollection.findOne(ObjectId(insertedId))
-
-    return jwt.sign({ userId }, process.env.JWT_SECRET)
-  }
-}
+  { Auth }
+) => Auth.register(email, password, firstName, lastName)
 
 module.exports = register
