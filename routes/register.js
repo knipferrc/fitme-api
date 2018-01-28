2c44fdde3e1cba3c259bf3456ddfe47a9de1cd3c
alexknipfer <alexanderknipfer@gmail.com>
2018-01-28 16:01:09 -0600

    Update register route

--- routes/register.js
+++ routes/register.js
@@ -1,17 +1,48 @@
 const router = require('express').Router()
-const Auth = require('../models/Auth')
+const jwt = require('jsonwebtoken')
 
-router.post('/', async req => {
-  const AuthModel = new Auth(req.app.locals.db)
-  AuthModel.setCollection('users')
+const User = require('../mongooseModels/User')
 
+router.post('/', async (req, res) => {
   const { email, password, firstName, lastName } = req.body
 
-  try {
-    return AuthModel.register(email, password, firstName, lastName)
-  } catch (error) {
-    throw new Error(error.message)
+  const foundUser = await User.findOne({ email })
+
+  if (foundUser) {
+    return res.status(403).json({
+      success: false,
+      message: 'Email is already in use'
+    })
   }
+
+  const newUser = new User({ email, password, firstName, lastName })
+  const savedUser = await newUser.save()
+
+  const accessToken = jwt.sign(
+    { userId: savedUser._id },
+    process.env.JWT_SECRET
+  )
+
+  const user = {
+    accessToken,
+    role: savedUser.role,
+    email: savedUser.email,
+    firstName: savedUser.firstName,
+    lastName: savedUser.lastName
+  }
+
+  req.logIn(user, err => {
+    if (err) {
+      return res.status(400).json({
+        success: false,
+        message: err.message
+      })
+    }
+    return res.json({
+      success: true,
+      user
+    })
+  })
 })
 
 module.exports = router
