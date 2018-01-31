065a0d8578c321d721d41a56a2336cca2594fdb3
alexknipfer <alexanderknipfer@gmail.com>
2018-01-30 22:11:51 -0600

    Remove test subscription

--- subscriptions/userRegistered.js
+++
@@ -1,9 +0,0 @@
-const pubsub = require('../utils/pubsub')
-
-const USER_REGISTERED = 'user_registered'
-
-const userRegistered = {
-  subscribe: () => pubsub.asyncIterator(USER_REGISTERED)
-}
-
-module.exports = userRegistered
