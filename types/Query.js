3e86c08c06e76c7e295cd831a488bc969c7c647e
knipferrc <knipferrc@gmail.com>
2018-02-04 16:56:39 -0500

    not needed

--- types/Query.js
+++ types/Query.js
@@ -3,7 +3,7 @@ const Query = `
     currentUser(accessToken: String!): User
     trainersTotalExercises(trainerId: ID!): Int
     trainersTotalWorkouts(trainerId: ID!): Int
-    trainersNextAppointment(trainerId: ID!): Appointment,
+    trainersNextAppointment(trainerId: ID!): Appointment
     trainersTotalClients(trainerId: ID!): Int
     allTrainers: [User] @isAuthenticated
     trainersClients(trainerId: ID!): [User]
