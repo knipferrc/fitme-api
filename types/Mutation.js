adbcce25a3f2dcac221feada9e2e954601cd7a94
alexknipfer <alexanderknipfer@gmail.com>
2018-02-04 17:20:02 -0600

    Return user on reset password

--- types/Mutation.js
+++ types/Mutation.js
@@ -3,7 +3,7 @@ const Mutation = `
     login(email: String!, password: String!): User
     register(email: String!, password: String!, firstName: String!, lastName: String!): User
     sendResetPasswordEmail(email: String!): Boolean
-    resetPassword(password: String!, token: String!): String
+    resetPassword(password: String!, token: String!): User
     createClient(email: String!, password: String!, firstName: String!, lastName: String!, trainerId: ID!): User
   }
 `
