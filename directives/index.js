499e7e687274f395aa7f9090a83e97075b39c098
alexknipfer <alexanderknipfer@gmail.com>
2018-02-08 20:40:45 -0600

    Use accesstoken

--- directives/index.js
+++ directives/index.js
@@ -3,17 +3,14 @@ const jwt = require('jsonwebtoken')
 
 const directiveResolvers = {
   isAuthenticated(result, source, args, context) {
-    const token = context.headers.authorization
+    const token = context.accessToken
     if (!token) {
       throw new AuthorizationError({
         message: 'You must supply a JWT for authorization'
       })
     }
     try {
-      const decoded = jwt.verify(
-        token.replace('Bearer ', ''),
-        process.env.JWT_SECRET
-      )
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
       return result
     } catch (error) {
       throw new AuthorizationError()
